22f0610103a0a360b8bed1f41194df38c69c046e
jbenavidesnati05 <jorgenati05@gmail.com>
2022-05-01 16:19:59 -0500

    mas arrays

--- array.js
+++ array.js
@@ -21,3 +21,28 @@ var n_elementos = meses.length;
 document.write("<br>");
 document.write(` El numero de elementos que compone el array es ${n_elementos}`);
 
+// metodo lentgh 
+document.write("<br>")
+
+let amigos=["andres","jacob","anto"];
+amigos[amigos.length] = "antonio";
+document.write(amigos)
+
+// metodo push para agregar elementos al final del array 
+
+amigos.push("antony","fercho");
+document.write(amigos)
+document.write("<br>")  
+
+// metedo join para agregar espaciados 
+
+document.write(amigos.join("***"));
+
+// sort para ordenar alfabeticamente 
+
+var ordenados = amigos.sort();  
+document.write("<br>")
+document.write(ordenados);
+
+
+
